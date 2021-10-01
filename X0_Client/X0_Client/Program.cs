197aa222c528ddc158acb4ca1b2838ecf9b92cdc
i-geeg-i <glebov.el2005@yandex.ru>
2021-10-01 18:38:52 +0500

    v 0.51

--- X0_Client/X0_Client/Program.cs
+++ X0_Client/X0_Client/Program.cs
@@ -181,7 +181,7 @@ namespace X0_Client
             IPAddress ip = IPAddress.Parse("127.0.0.1"); //chose ip
             IPEndPoint addr = new IPEndPoint(ip, 1337); //chose addres
             sock.Connect(addr);//connect addres and socket
-            bool x = false;
+            bool IsWeX = false;
             bool wePlayNow = false;
             int[] map = new int[9]; //creating map of the game (maybe it should be in another class)
             while (true)
